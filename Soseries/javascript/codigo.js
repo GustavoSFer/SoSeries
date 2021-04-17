c574997a06e2bf5eb01522489f4a7eca4009d1ea
GustavoSFer <gustavo.sfernander@gmail.com>
2021-04-16 21:42:06 -0300

    Adicionando projeto

---
+++ Soseries/javascript/codigo.js
@@ -0,0 +1,11 @@
+ function trailer(){
+ 	document.getElementById('rodarTrailer').className = 'trailer2'
+ 	document.getElementById('rodarTrailer').innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZHaokMTWgdY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+ 	//window.open('https://www.youtube.com/watch?v=jMIrycwfrDA')
+ }
+ function informacao(){
+ 	document.getElementById('maisInfomacao').className = 'info'
+ 	document.getElementById('maisInfomacao').innerHTML = '<p>Após os eventos de "Vingadores: Ultimato", Sam Wilson/Falcão e Bucky Barnes/Soldado Invernal se unem em uma aventura que testa suas habilidades - e a paciência.</p> <p>A 1ª temporada de Falcão e o Soldado Invernal começou a ser exibida em 19 de março de 2021.</p><br> <p class="sinopse1">é uma minissérie estadunidense criada para o Disney+ por Malcolm Spellman, baseada nos personagens da Marvel Comics Sam Wilson/Falcão e Bucky Barnes/Soldado Invernal. A série é ambientada no Universo Cinematográfico Marvel (UCM), compartilhando continuidade com os filmes da franquia. Os eventos da série acontecem após o filme Avengers: Endgame (2019). A série é produzida pela Marvel Studios, com Spellman atuando como roteirista principal e Kari Skogland dirigindo.</p>'
+ 	//window.open('informacoes.html')
+ }
